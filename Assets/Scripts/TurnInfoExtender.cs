02001f1facfe112713ad081858bd9db91e16f49c
pPrecel <filip.strozik@outlook.com>
2019-01-26 20:00:54 +0100

    Add text information about players' turn

---
+++ Assets/Scripts/TurnInfoExtender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class TurnInfoExtender : MonoBehaviour
+    {
+        public Text infoField;
+
+        public string redTurn = "Turn: Red";
+        public string blueTurn = "Turn Blue";
+
+        public void Start()
+        {
+            infoField.text = redTurn;
+        }
+
+            public void updateField()
+        {
+            if (infoField.text.Equals(redTurn))
+                infoField.text = blueTurn;
+            else
+                infoField.text = redTurn;
+        }
+    }
+}
